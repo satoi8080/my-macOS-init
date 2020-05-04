11dddf88bcb922c97aeabd171003eb77ee91721f
Satoi8080 <satoi8080@users.noreply.github.com>
2020-05-04 14:51:35 +0900

    Add files via upload

---
+++ macOS-init.sh
@@ -0,0 +1,51 @@
+#!bin/bash
+
+defaults write com.apple.dock slow-motion-allowed -bool YES && killall Dock
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew update
+
+# The following cask Formulaes could be found at https://formulae.brew.sh
+
+brew cask install iterm2
+brew cask install github
+brew cask install notion
+brew cask install discord
+brew cask install jetbrains-toolbox
+brew cask install balenaetcher
+brew cask install google-chrome
+brew cask install google-backup-and-sync
+brew cask install imageoptim
+brew cask install iina
+# brew cask install postman
+# brew cask install edex-ui
+
+# IoT apps
+
+# brew cask install arduino
+# brew cask install fritzing
+# brew cask install raspberry-pi-imager
+
+# Network apps
+
+# brew cask install outline-manager
+# brew cask install surge
+# brew cask install shadowsocksx
+
+# Writing apps
+
+# brew cask install microsoft-office
+# brew cask install writefull
+# brew cask install youdaodict
+
+# Management apps
+
+# brew cask install imazing
+# brew cask install profilecreator
+# brew cask install synergy
+# brew cask install cryptomator
+# brew cask install karabiner-elements
+
+brew install sl
+brew install mas
---
+++ macOS-mas.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+# Mac App Store apps
+
+brew install mas
+
+mas install 497799835 # Xcode
+mas install 1024640650 # CotEditor
+mas install 412448059 # ForkLift
